ff96a1939a2051babede8b7d1fc6a61f5e47b0ba
savan <savankhedekar97@gmail.com>
2025-05-01 13:37:55 +0530

    backup

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,10 @@
+import React from 'react';
 import './Footer.css';
-import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa';
 
-const Footer = () => {
+export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-glass">
-        <div className="footer-columns">
-          
-          {/* Column 1: About */}
-          <div className="footer-col">
-            <h3>About NAOI</h3>
-            <p>National Association of Optimist India (NAOI) promotes sailing, organizes events, and nurtures young talent in the Optimist class.</p>
-          </div>
-
-          {/* Column 2: Quick Links */}
-          <div className="footer-col">
-            <h3>Quick Links</h3>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/videos">Videos</a></li>
-              <li><a href="/images">Images</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </div>
-
-          {/* Column 3: Social Media */}
-          <div className="footer-col">
-            <h3>Follow Us</h3>
-            <div className="footer-social">
-              <a href="#"><FaFacebookF /></a>
-              <a href="#"><FaInstagram /></a>
-              <a href="#"><FaYoutube /></a>
-              <a href="#"><FaTwitter /></a>
-            </div>
-          </div>
-
-        </div>
-        <hr />
-        <p className="footer-copy">© {new Date().getFullYear()} NAOI. All rights reserved.</p>
-      </div>
+      <p>© {new Date().getFullYear()} NAOI Website. All rights reserved.</p>
     </footer>
   );
-};
-
-export default Footer;
+}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,71 +1,50 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar() {
+export default function Navbar() {
   return (
-    <div className="menu-bar sticky-top">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
-        <div className="container">
-          <Link className="navbar-brand logo-text" to="/">NAOI</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li><NavLink to="/" className="nav-link">Home</NavLink></li>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
+        <li className="nav-item mega-menu">
+          <span className="nav-link">Menu</span>
+          <div className="mega-content">
+            
+            <div className="mega-column">
+              <h4>Events</h4>
+              <ul>
+                <li><NavLink to="/domestic-sailing-calendar">Domestic Sailing Calendar</NavLink></li>
+                <li><NavLink to="/nor">NOR</NavLink></li>
+                <li><NavLink to="/results">Results</NavLink></li>
+                <li><NavLink to="/view-calendar">View Calendar</NavLink></li>
+              </ul>
+            </div>
+            
+            <div className="mega-column">
+              <h4>Multimedia</h4>
+              <ul>
+                <li><NavLink to="/images">Photo Gallery</NavLink></li>
+                <li><NavLink to="/videos">Videos</NavLink></li>
+              </ul>
+            </div>
+            
+            <div className="mega-column">
+              <h4>Documents &amp; Rules</h4>
+              <ul>
+                <li><NavLink to="/constitution">Constitution</NavLink></li>
+                <li><NavLink to="/optimist-rules">Optimist Rules</NavLink></li>
+                <li><NavLink to="/forms/new-club-membership">New Club Membership</NavLink></li>
+                <li><NavLink to="/forms/issue-opti-sail-number">Issue of OpTi Sail Number</NavLink></li>
+              </ul>
+            </div>
 
-              <li className="nav-item dropdown hover-dropdown">
-                <span className="nav-link dropdown-toggle" role="button">Events</span>
-                <ul className="dropdown-menu animate__animated animate__fadeInDown">
-                  <li><Link className="dropdown-item" to="/domestic-sailing-calendar">Domestic Sailing Calendar</Link></li>
-                  <li><Link className="dropdown-item" to="/nor">NOR</Link></li>
-                  <li><Link className="dropdown-item" to="/results">Results</Link></li>
-                </ul>
-              </li>
-
-              <li className="nav-item dropdown hover-dropdown">
-                <span className="nav-link dropdown-toggle" role="button">Multimedia</span>
-                <ul className="dropdown-menu animate__animated animate__fadeInDown">
-                  <li><Link className="dropdown-item" to="/images">Images</Link></li>
-                  <li><Link className="dropdown-item" to="/videos">Videos</Link></li>
-                </ul>
-              </li>
-
-              <li className="nav-item dropdown hover-dropdown">
-                <span className="nav-link dropdown-toggle" role="button">Documents and Rules</span>
-                <ul className="dropdown-menu animate__animated animate__fadeInDown">
-                  
-                  <li><Link className="dropdown-item" to="/constitution">Constitution</Link></li>
-                  
-                  <li className="dropdown-submenu hover-submenu">
-                    <span className="dropdown-item dropdown-toggle" role="button">Forms</span>
-                    <ul className="dropdown-menu animate__animated animate__fadeInDown">
-                      <li><Link className="dropdown-item" to="/forms/new-club-membership">New Club Membership Form</Link></li>
-                      <li><Link className="dropdown-item" to="/forms/issue-opti-sail-number">Issue of OPTI Sail Number Form</Link></li>
-                    </ul>
-                  </li>
-
-                  <li><Link className="dropdown-item" to="/optimist-rules">Optimist Class Rules</Link></li>
-                </ul>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/news">News</Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-              </li>
-
-            </ul>
           </div>
-        </div>
-      </nav>
-    </div>
+        </li>
+
+        <li><NavLink to="/contact" className="nav-link">Contact</NavLink></li>
+      </ul>
+    </nav>
   );
 }
-
-export default Navbar;
